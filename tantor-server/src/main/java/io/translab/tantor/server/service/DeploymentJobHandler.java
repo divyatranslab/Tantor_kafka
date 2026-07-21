fa12ee8a7e4a12d2a477a7eec2cf241e88406f9b
yawar2-ux <yawar.a@translab.io>
2026-07-21 11:01:40 +0530

    refactor: remove Kafka exporter reconciliation from DeploymentJobHandler and ExternalClusterService

--- tantor-server/src/main/java/io/translab/tantor/server/service/DeploymentJobHandler.java
+++ tantor-server/src/main/java/io/translab/tantor/server/service/DeploymentJobHandler.java
@@ -93,7 +93,6 @@ public class DeploymentJobHandler implements JobHandler {
 
         cluster.setStatus("SUCCESS");
         clusterRepository.save(cluster);
-        prometheusMonitoringService.ensureKafkaExporter(cluster.getId());
     }
 
     private void executeDeployBatch(
